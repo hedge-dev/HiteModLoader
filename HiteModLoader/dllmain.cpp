c1e8f6b5e26e25831c61e6327926855f0615720d
SuperSonic16 <25761885+thesupersonic16@users.noreply.github.com>
2022-06-29 02:45:49 +1000

    Fixed lockup on startup

--- HiteModLoader/dllmain.cpp
+++ HiteModLoader/dllmain.cpp
@@ -1,10 +1,23 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 #include "Loader.h"
+#include "Helpers.h"
 
 #pragma comment(linker, "/EXPORT:D3D11CreateDevice=C:\\Windows\\System32\\d3d11.D3D11CreateDevice")
 #pragma comment(linker, "/EXPORT:D3D11CoreCreateDevice=C:\\Windows\\System32\\d3d11.D3D11CoreCreateDevice")
 #pragma comment(linker, "/EXPORT:D3D11CreateDeviceAndSwapChain=C:\\Windows\\System32\\d3d11.D3D11CreateDeviceAndSwapChain")
+
+static bool Loaded = false;
+
+HOOK(void, __fastcall, GetStartupInfoWHook, PROC_ADDRESS("Kernel32.dll", "GetStartupInfoW"), void* lpStartupInfo)
+{
+    if (!Loaded)
+    {
+        Loaded = true;
+        InitLoaders();
+    }
+    originalGetStartupInfoWHook(lpStartupInfo);
+}
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -14,7 +27,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        InitLoaders();
+        INSTALL_HOOK(GetStartupInfoWHook);
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
