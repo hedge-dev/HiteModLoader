5646a24436f53e724167f68f9e6202a6a18ee01f
SuperSonic16 <25761885+thesupersonic16@users.noreply.github.com>
2022-07-02 16:34:49 +1000

    Added script error popup

--- HiteModLoader/LoggingHandler.cpp
+++ HiteModLoader/LoggingHandler.cpp
@@ -1,11 +1,19 @@
 #include "pch.h"
+#include "LoggingHandler.h"
 #include "HiteModLoader.h"
 #include "Helpers.h"
 #include "SigScan.h"
 
-HOOK(int, __fastcall, Print1, SigPrint1(), __int64 a1, const char* format, const char* p1, const char* p2, const char* p3)
+HOOK(int, __fastcall, Print1, SigPrint1(), RetroPrintModes printMode, const char* format, const char* p1, const char* p2, const char* p3)
 {
-    return printf((std::string("[Sonic Origins] ") + format).c_str(), p1, p2, p3);
+    // Display error message on script error
+    if (printMode == PRINTMODE_SCRIPTERROR)
+    {
+        char buffer[0x400] = {};
+        sprintf(buffer, format, p1, p2, p3);
+        MessageBoxA(NULL, buffer, "Script Error", MB_ICONERROR);
+    }
+    return printf((std::string("[Engine] ") + format).c_str(), p1, p2, p3);
 }
 
 void InitLoggingHandler()
--- HiteModLoader/LoggingHandler.h
+++ HiteModLoader/LoggingHandler.h
@@ -1,3 +1,12 @@
 #pragma once
 
+enum RetroPrintModes
+{
+	PRINTMODE_NORMAL,
+	PRINTMODE_POPUP,
+	PRINTMODE_ERROR,
+	PRINTMODE_FATAL,
+	PRINTMODE_SCRIPTERROR
+};
+
 extern void InitLoggingHandler();
