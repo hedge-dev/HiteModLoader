a2f3c036d40b162a6a0faeb737f245b0aa93838f
SuperSonic16 <25761885+thesupersonic16@users.noreply.github.com>
2022-10-19 21:38:18 +1100

    Display new line only on a script error.

--- HiteModLoader/LoggingHandler.cpp
+++ HiteModLoader/LoggingHandler.cpp
@@ -7,6 +7,8 @@
 
 HOOK(int, __fastcall, Print1, SigPrint1(), RetroPrintModes printMode, const char* format, const char* p1, const char* p2, const char* p3)
 {
+    bool newLine = false;
+
     // Display error message on script error
     if (printMode == PRINTMODE_SCRIPTERROR)
     {
@@ -14,10 +16,13 @@ HOOK(int, __fastcall, Print1, SigPrint1(), RetroPrintModes printMode, const char
         sprintf(buffer, format, p1, p2, p3);
         MessageBoxA(NULL, buffer, "Script Error", MB_ICONERROR);
 
+        // Add new line as this message is not intended to be written to a console
+        newLine = true;
+
         // Return to title screen
         ReturnToTitle = true;
     }
-    return printf((std::string("[Engine] ") + format + "\n").c_str(), p1, p2, p3);
+    return printf((std::string("[Engine] ") + format + (newLine ? "\n" : "")).c_str(), p1, p2, p3);
 }
 
 void InitLoggingHandler()
